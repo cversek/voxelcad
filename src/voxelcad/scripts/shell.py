6600ff0b40218a3158fd6b8459ba585aee379d26
cversek <cversek@gmail.com>
2021-11-22 16:07:29 -0500

    have the voxelcad_shell use its own profile to separate it from other IPython sessions; import more new features

--- src/voxelcad/scripts/shell.py
+++ src/voxelcad/scripts/shell.py
@@ -1,5 +1,6 @@
 ################################################################################
 import logging, sys, argparse
+LOGGER = logging.getLogger(__name__)
 
 import IPython
 
@@ -16,23 +17,28 @@ def launch_shell(debug=False):
         print(__BANNER)
         #configure and pop open ipython terminal
         if debug:
-            logging.getLogger().setLevel(logging.DEBUG)
+            logging.basicConfig(level=logging.DEBUG)
+            LOGGER.debug("DEBUG MODE ON")
+
         else:
-            logging.getLogger().setLevel(logging.INFO)
+            logging.basicConfig(level=logging.INFO)
                
         from traitlets.config.loader import Config
         cfg = Config()
+        #choosing a dedicate profile separates history and config from other IPython sessions
+        cfg.BaseIPythonApplication.profile='voxelcad_shell' 
         cfg.InteractiveShellApp.exec_lines = [
              #"%pylab tk",
              "import voxelcad.environment as ENV",
              "from voxelcad.cube import Cube",
              "from voxelcad.sphere import Sphere",
              "from voxelcad.cylinder import Cylinder",
-             "from voxelcad.gyroid_cube import GyroidCube",
+             "from voxelcad.gyroid_cube import GyroidCube, WigglyGyroidCube",
+             "from voxelcad.voxel_model import union_all",
         ]
         IPython.start_ipython(argv=['--pylab'],config = cfg)
     finally:
-        logging.getLogger().setLevel(logging.DEBUG)
+        LOGGER.setLevel(logging.DEBUG)
         
 ###############################################################################
 # Main
@@ -50,5 +56,4 @@ def main():
                         help    = "trigger debugging mode"
                        )
     args = parser.parse_args()
-
     launch_shell(debug=args.debug)
