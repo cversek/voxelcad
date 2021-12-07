4aaabb9485e68a54212947fdb694762a17cd57ee
cversek <cversek@gmail.com>
2021-12-06 21:22:52 -0500

    refactoring

--- src/voxelcad/cube.py
+++ src/voxelcad/cube.py
@@ -10,34 +10,35 @@ from voxelcad.debug import currentframe, DEBUG_TAG, DEBUG_EMBED
 class Cube(VoxelModel):
     def __init__(self, size, res=None, center=False, **kwargs):
         super().__init__(**kwargs)
-        self.size_vector = sv = np.array(size)*np.ones(3)
-        if res is None:
-            res = ENV.res
-        self.res_vector  = (np.array(res)*np.ones(3)).astype('uint')
+        self.size = np.array(size)*np.ones(3)
         self.center = center
         #set up grid dimensions
-        sv = self.size_vector
+        if res is None:
+            res = ENV.res
+        res_vector  = (np.array(res)*np.ones(3)).astype('uint')
         if self.center:
-            sx,sy,sz = sv/2
+            sx,sy,sz = self.size/2
             self.grid = VoxelGrid(xlim=(-sx,sx),
                                   ylim=(-sy,sy),
                                   zlim=(-sz,sz),
-                                  res=self.res_vector)
+                                  res=res_vector)
         else:
-            sx,sy,sz = sv
+            sx,sy,sz = self.size
             self.grid = VoxelGrid(xlim=(0,sx),
                                   ylim=(0,sy),
                                   zlim=(0,sz),
-                                  res=self.res_vector)
+                                  res=res_vector)
         
     def render_volume(self):
         super().render_volume() # will construct_grid if it is None
         # fill all of the cubic volume between the margins
         X,Y,Z,V,m = self.grid.construct_mesh()
         #DEBUG_TAG(currentframe());DEBUG_EMBED(local_ns=locals(),global_ns=globals())
-        V[m:-m,m:-m,m:-m] = (X <= self.size_vector[0]) & \
-                            (Y <= self.size_vector[1]) & \
-                            (Z <= self.size_vector[2]) 
+        sx,sy,sz = self.size
+        cx,cy,cz = self.grid.compute_center_vector()
+        V[m:-m,m:-m,m:-m] = (np.abs(X-cx) <= sx/2) &\
+                            (np.abs(Y-cy) <= sy/2) &\
+                            (np.abs(Z-cz) <= sz/2)
         self.voxel_data = V
         return self.voxel_data
 
