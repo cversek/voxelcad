5eebb7621d4f73be62b9f6f6f1b335ad97769a59
cversek <cversek@gmail.com>
2022-01-18 12:33:10 -0500

    put useful names up-front for ease of import from package

---
+++ src/voxelcad/__init__.py
@@ -0,0 +1,6 @@
+from voxelcad.cube import Cube
+from voxelcad.sphere import Sphere
+from voxelcad.cylinder import Cylinder
+from voxelcad.gyroid_cube import GyroidCube
+
+import voxelcad.environment as ENV
