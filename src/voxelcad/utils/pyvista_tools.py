9ae6e37f060eefe8a1919442dd4ae249581e1daa
cversek <cversek@gmail.com>
2023-07-19 12:50:55 -0400

    allow spec of 0 smooth_iters to skip smoothing step

--- src/voxelcad/utils/pyvista_tools.py
+++ src/voxelcad/utils/pyvista_tools.py
@@ -1,5 +1,6 @@
 def downscale_trimesh(trimesh, repeat=1, smooth_iters = 100, decimation_factor=0.5):
     for i in range(repeat):
-        trimesh = trimesh.smooth(n_iter=smooth_iters, progress_bar=True)
+        if smooth_iters > 0:
+            trimesh = trimesh.smooth(n_iter=smooth_iters, progress_bar=True)
         trimesh = trimesh.decimate_pro(decimation_factor,progress_bar=True)
     return trimesh
